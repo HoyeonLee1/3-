21d9d289db37d25dbb20c552f5fe5753cfb0e3ab
HoyeonLee1 <hoyon95@naver.com>
2022-11-07 17:46:52 +0900

    [10주차]과제 주석 포함 수정

--- 10w/10w.cpp
+++ 10w/10w.cpp
@@ -1,13 +1,13 @@
-#include <chrono>
-#include <thread>
-#include <string>
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
 #include "Musoenmath.h"
+
 using namespace std;
+
 void mat3::printmat3()
 {
+    //mat3 출력용 함수 본문
     cout << "----------------" << endl;
     for (int i = 0; i < 3; i++)
     {
@@ -21,6 +21,7 @@ void mat3::printmat3()
 }
 void vec3::printvec3()
 {
+    //vec3 출력용 함수 본문
     cout << "----------------" << endl;
 
     cout << x << " " << y << " " << z << " ";
@@ -31,29 +32,35 @@ void vec3::printvec3()
 }
 int main()
 {
-
+    //삼각함수 선언
     const float PI = 3.1415926;
     double a = sin(30 * PI / 180);
     double b = cos(30 * PI / 180);
 
+    //좌표 초기값(1,1,1) 초기화
     vec3 v = vec3(1, 1, 1);
 
+    //(3,5)이동행렬 초기화
     mat3 Tr = mat3(1.0, 0.0, 0.0,
         0.0, 1.0, 0.0,
         3.0, 5.0, 1.0);
-
+    //30도 회전행렬 초기화
     mat3 Ro = mat3(b, -a, 0.0,
         a, b, 0.0,
         0.0, 0.0, 1.0);
+    //2배 확대 스케일 행렬 초기화
     mat3 Sc = mat3(2.0, 0.0, 0.0,
         0.0, 2.0, 0.0,
         0.0, 0.0, 1.0);
 
+    //행렬이 잘 초기화 되었는지 확인하는 출력문
     v.printvec3();
     Tr.printmat3();
     Ro.printmat3();
     Sc.printmat3();
+    //결과값 좌표 = 초기값 * 이동행렬 * 회전행렬 * 스케일행렬
     vec3 p = v * Tr * Ro * Sc;
+    //최종 좌표값 출력
     p.printvec3();
 
 }
--- 10w/MusoenMath.h
+++ 10w/MusoenMath.h
@@ -1,38 +1,49 @@
 #pragma once
 
 
+
+//3개의 좌표값을 가지는 vec3타입
 struct vec3
 {
 	float x;
 	float y;
 	float z;
 
+
+	// 초기값 설정
 	vec3()
 	{
 		x = 0;
 		y = 0;
 		z = 0;
 	}
+
+	//초기화용
 	vec3(float x_, float y_, float z_)
 	{
 		x = x_;
 		y = y_;
 		z = z_;
 	}
+	//vec3 타입 출력용 함수
 	void printvec3();
 };
 
+
+//vec3 타입끼리 곱셈연산을 하게해주는 연산자 오버로딩
 vec3 operator*(vec3 a, vec3 b)
 {
 	vec3 result(a.x * b.x, a.y * b.y, a.z * b.z);
 	return result;
 }
 
+
+//3*3 행렬 선언
 struct mat3
 {
 	float a[3][3];
 
-
+	//초기값
 	mat3()
 	{
 		a[0][0] = 0;
@@ -46,6 +57,8 @@ struct mat3
 		a[2][2] = 0;
 
 	}
+
+	//초기화용
 	mat3(float a_, float b_, float c_, float d_, float e_, float f_, float g_, float h_, float i_)
 	{
 		a[0][0] = a_;
@@ -58,10 +71,11 @@ struct mat3
 		a[2][1] = h_;
 		a[2][2] = i_;
 	}
-	void printmat3();
+	void printmat3();//mat3 출력용 함수
 
 };
 
+//mat3 타입끼리 연산을 가능하게 해주는 연산자 오버로딩
 mat3 operator*(mat3 a, mat3 b)
 {
 	mat3 r;
@@ -81,8 +95,7 @@ mat3 operator*(mat3 a, mat3 b)
 	return r;
 }
 
-
-
+//vec3 타입과 mat3 타입간 연산을 가능하게 해주는 연산자 오버로딩
 vec3 operator*(vec3 a, mat3 b)
 {
 	vec3 r;
