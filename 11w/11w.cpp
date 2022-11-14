6bff2dceb3dd833ac0f7507bb8fe67ede80f8826
HoyeonLee1 <hoyon95@naver.com>
2022-11-14 20:39:05 +0900

    [11주차] 중간과제2 주석추가

--- 11w/11w.cpp
+++ 11w/11w.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//3x3 행렬 출력함수
 void mat3::printmat3()
 {
     cout << "----------------" << endl;
@@ -15,6 +16,7 @@ void mat3::printmat3()
     cout << "----------------" << endl;
     cout << endl;
 }
+//4x4 행렬 출력함수
 void mat4::printmat4()
 {
     cout << "----------------" << endl;
@@ -27,6 +29,8 @@ void mat4::printmat4()
     cout << "----------------" << endl;
     cout << endl;
 }
+
+//1x3 벡터 출력 함수
 void vec1x3::printvec1x3()
 {
     cout << "----------------" << endl;
@@ -35,6 +39,8 @@ void vec1x3::printvec1x3()
     cout << "----------------" << endl;
     cout << endl;
 }
+
+//vec3 출력 함수
 void vec3::printvec3()
 {
     cout << "----------------" << endl;
@@ -43,6 +49,8 @@ void vec3::printvec3()
     cout << "----------------" << endl;
     cout << endl;
 }
+
+//vec4 출력 함수
 void vec4::printvec4()
 {
     cout << "----------------" << endl;
@@ -51,6 +59,7 @@ void vec4::printvec4()
     cout << "----------------" << endl;
     cout << endl;
 }
+//vec4 출력 함수
 void vec1x4::printvec1x4()
 {
     cout << "----------------" << endl;
@@ -59,6 +68,7 @@ void vec1x4::printvec1x4()
     cout << "----------------" << endl;
     cout << endl;
 }
+//3x3 행렬을 단위행렬로 초기화하는 함수
 void mat3::Mat3identity()
 {
     a[0][0] = 1;
@@ -73,6 +83,7 @@ void mat3::Mat3identity()
     a[2][1] = 0;
     a[2][2] = 1;
 }
+//4x4 행렬을 단위행렬로 초기화하는 함수
 void mat4::Mat4Identity()
 {
     b[0][0] = 1;
@@ -95,7 +106,7 @@ void mat4::Mat4Identity()
     b[3][2] = 0;
     b[3][3] = 1;
 }
-
+// 3x3행렬을 전치하는 함수
 void mat3::Mat3Transpose()
 {
 
@@ -114,6 +125,7 @@ void mat3::Mat3Transpose()
 
             a[j][i] = p.a[i][j];
 }
+//4x4 행렬을 전치하는 함수
 void mat4::Mat4Transpose()
 {
 
@@ -132,7 +144,7 @@ void mat4::Mat4Transpose()
 
             b[j][i] = p.b[i][j];
 }
-
+//3x3 행렬을 다른 행렬에 대입할 때 쓰는 함수
 void mat3::operator=(const mat3& x)
 
 {
@@ -144,6 +156,7 @@ void mat3::operator=(const mat3& x)
             a[i][j] = x.a[i][j];
 
 }
+//4x4 행렬을 다른 행렬에 대입할 때 쓰는 함수
 void mat4::operator=(const mat4& x)
 
 {
@@ -153,69 +166,69 @@ void mat4::operator=(const mat4& x)
 
             b[i][j] = x.b[i][j];
 }
+//3x3 행렬 두 개를 전치 후 곱셈을 진행하여 그 결과값을 리턴해주는 함수
 void mat3::multiplytransposematrix(mat3 x, mat3 y) {
     mat3  v, p;
-
+    //x행렬 전치
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             v.a[j][i] = x.a[i][j];
-
+    //y행렬 전치
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             p.a[j][i] = y.a[i][j];
 
+    //1행
     a[0][0] = (v.a[0][0] * p.a[0][0]) + (v.a[0][1] * p.a[1][0]) + (v.a[0][2] * p.a[2][0]);
     a[0][1] = (v.a[0][0] * p.a[0][1]) + (v.a[0][1] * p.a[1][1]) + (v.a[0][2] * p.a[2][1]);
     a[0][2] = (v.a[0][0] * p.a[0][2]) + (v.a[0][1] * p.a[1][2]) + (v.a[0][2] * p.a[2][2]);
   
-
+    //2행
     a[1][0] = (v.a[1][0] * p.a[0][0]) + (v.a[1][1] * p.a[1][0]) + (v.a[1][2] * p.a[2][0]);
     a[1][1] = (v.a[1][0] * p.a[0][1]) + (v.a[1][1] * p.a[1][1]) + (v.a[1][2] * p.a[2][1]);
     a[1][2] = (v.a[1][0] * p.a[0][2]) + (v.a[1][1] * p.a[1][2]) + (v.a[1][2] * p.a[2][2]);
     
-
+    //3행
     a[2][0] = (v.a[2][0] * p.a[0][0]) + (v.a[2][1] * p.a[1][0]) + (v.a[2][2] * v.a[2][0]);
     a[2][1] = (v.a[2][0] * p.a[0][1]) + (v.a[2][1] * p.a[1][1]) + (v.a[2][2] * v.a[2][1]);
     a[2][2] = (v.a[2][0] * p.a[0][2]) + (v.a[2][1] * p.a[1][2]) + (v.a[2][2] * v.a[2][2]);
-   
-
-
 }
+//4x4 행렬 두 개를 전치 후 곱셈을 진행하여 그 결과값을 리턴해주는 함수
 void mat4::multiplytransposematrix(mat4 x, mat4 y) {
     mat4  v, p;
-
+    //x행렬 전치
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             v.b[j][i] = x.b[i][j];
-
+    //y행렬 전치
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             p.b[j][i] = y.b[i][j];
-
+    //1행
     b[0][0] = (v.b[0][0] * p.b[0][0]) + (v.b[0][1] * p.b[1][0]) + (v.b[0][2] * p.b[2][0]) + (v.b[0][3] * p.b[3][0]);
     b[0][1] = (v.b[0][0] * p.b[0][1]) + (v.b[0][1] * p.b[1][1]) + (v.b[0][2] * p.b[2][1]) + (v.b[0][3] * p.b[3][1]);
     b[0][2] = (v.b[0][0] * p.b[0][2]) + (v.b[0][1] * p.b[1][2]) + (v.b[0][2] * p.b[2][2]) + (v.b[0][3] * p.b[3][2]);
     b[0][3] = (v.b[0][0] * p.b[0][3]) + (v.b[0][1] * p.b[1][3]) + (v.b[0][2] * p.b[2][3]) + (v.b[0][3] * p.b[3][3]);
-
+    //2행
     b[1][0] = (v.b[1][0] * p.b[0][0]) + (v.b[1][1] * p.b[1][0]) + (v.b[1][2] * p.b[2][0]) + (v.b[1][3] * p.b[3][0]);
     b[1][1] = (v.b[1][0] * p.b[0][1]) + (v.b[1][1] * p.b[1][1]) + (v.b[1][2] * p.b[2][1]) + (v.b[1][3] * p.b[3][1]);
     b[1][2] = (v.b[1][0] * p.b[0][2]) + (v.b[1][1] * p.b[1][2]) + (v.b[1][2] * p.b[2][2]) + (v.b[1][3] * p.b[3][2]);
     b[1][3] = (v.b[1][0] * p.b[0][3]) + (v.b[1][1] * p.b[1][3]) + (v.b[1][2] * p.b[2][3]) + (v.b[1][3] * p.b[3][3]);
-
+    //3행
     b[2][0] = (v.b[2][0] * p.b[0][0]) + (v.b[2][1] * p.b[1][0]) + (v.b[2][2] * v.b[2][0]) + (v.b[2][3] * p.b[3][0]);
     b[2][1] = (v.b[2][0] * p.b[0][1]) + (v.b[2][1] * p.b[1][1]) + (v.b[2][2] * v.b[2][1]) + (v.b[2][3] * p.b[3][1]);
     b[2][2] = (v.b[2][0] * p.b[0][2]) + (v.b[2][1] * p.b[1][2]) + (v.b[2][2] * v.b[2][2]) + (v.b[2][3] * p.b[3][2]);
     b[2][3] = (v.b[2][0] * p.b[0][3]) + (v.b[2][1] * p.b[1][3]) + (v.b[2][2] * v.b[2][3]) + (v.b[2][3] * p.b[3][3]);
-
+    //4헹
     b[3][0] = (v.b[3][0] * p.b[0][0]) + (v.b[3][1] * p.b[1][0]) + (v.b[3][2] * v.b[2][0]) + (v.b[3][3] * p.b[3][0]);
     b[3][1] = (v.b[3][0] * p.b[0][1]) + (v.b[3][1] * p.b[1][1]) + (v.b[3][2] * v.b[2][1]) + (v.b[3][3] * p.b[3][1]);
     b[3][2] = (v.b[3][0] * p.b[0][2]) + (v.b[3][1] * p.b[1][2]) + (v.b[3][2] * v.b[2][2]) + (v.b[3][3] * p.b[3][2]);
     b[3][3] = (v.b[3][0] * p.b[0][3]) + (v.b[3][1] * p.b[1][3]) + (v.b[3][2] * v.b[2][3]) + (v.b[3][3] * p.b[3][3]);
-
 }
 
 int main()
 {
+    // 1. 단위행렬 초기화 출력
     mat3 mat3IdentityTest(0, 0, 0, 0, 0, 0, 0, 0, 0);
     mat4 mat4IdentityTest(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     mat3IdentityTest.Mat3identity();
@@ -223,6 +236,7 @@ int main()
     mat3IdentityTest.printmat3();
     mat4IdentityTest.printmat4();
 
+    //2. 전치행렬 출력
     mat3 mat3TransposeTest(1,2,3,1,2,3,1,2,3);
     mat4 mat4TransposeTest(1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4);
     mat3TransposeTest.printmat3();
@@ -232,6 +246,7 @@ int main()
     mat3TransposeTest.printmat3();
     mat4TransposeTest.printmat4();
     
+    //3. a) 행렬간 곱셈 출력
     mat3 mat3multiplyTest1(2, 0, 0, 0, 2, 0, 0, 0, 2);
     mat3 mat3multiplyTest2(6, 0, 0, 0, 6, 0, 0, 0, 6);
     mat4 mat4multiplyTest1(2, 0, 0,0, 0, 2, 0,0, 0, 0, 2,0,0,0,0,2);
@@ -241,6 +256,7 @@ int main()
     mat3multiplyResult.printmat3();
     mat4multiplyResult.printmat4();
 
+    //3. b) 두 행렬을 전치후 곱셈을 진행하는 함수 출력
     mat3 mat3multiplyTransposeTest1(2, 2, 2, 0, 0, 0, 0, 0, 0);
     mat3 mat3multiplyTransposeTest2(3, 3, 3, 0, 0, 0, 0, 0, 0);
     mat4 mat4multiplyTransposeTest1(3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -252,6 +268,7 @@ int main()
     mat4multiplyTransposeResult.multiplytransposematrix(mat4multiplyTransposeTest1, mat4multiplyTransposeTest2);
     mat4multiplyTransposeResult.printmat4();
 
+    //3. c)행렬과 벡터값의 곱셈 결과값 출력
     mat3 mat3multiplytest3(1, 0, 3, 0, 1, 5, 0, 0, 1);
     vec1x3 vec1x3multiplytest(1, 1, 1);
     vec3 vec3multiplytest(1, 1, 1);
@@ -267,6 +284,7 @@ int main()
     matvec4multiplyResult.printvec4();
     matvec1x4multiplyResult.printvec1x4();
     
+    //3. d) 행렬에 벡터를 곱했을 때 자동으로 전치 후 계산하는 연산자 오버로딩 출력 
     mat3 mat3multiplyTransposeTest3(1, 0, 3,    0, 1, 5,    0, 0, 1);
     vec3 vec3multiplyTransposeTest(1, 1, 1);
     vec1x3 vec1x3multyplyTransposeTest(1, 1, 1);
@@ -282,6 +300,7 @@ int main()
     vec1x4 vec1x4multiplyTransposeResult = mat4multiplyTransposeTest3 * vec1x4multyplyTransposeTest;
     vec1x4multiplyTransposeResult.printvec1x4();
 
+    //3. e-1) 행렬간 덧셈, 뺄셈
     mat3 mat3_P_M(4, 4, 4, 3, 3, 3, 2, 2, 2);
     mat3 mat3_P_M2(3, 3, 3, 2, 2,2, 1, 1, 1);
     mat4 mat4_P_M(5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2);
@@ -295,6 +314,33 @@ int main()
     mat4_P_M_Result = mat4_P_M - mat4_P_M2;
     mat4_P_M_Result.printmat4();
 
+    //3. e-2)벡터간 덧셈 뺄셈
+    vec3 vec3_P_M(4, 4, 4);
+    vec3 vec3_P_M2(3, 3, 3);
+    vec4 vec4_P_M(5, 5, 5, 5);
+    vec4 vec4_P_M2(4, 4, 4, 4);
+    vec3 vec3_P_M_Result = vec3_P_M + vec3_P_M2;
+    vec3_P_M_Result.printvec3();
+    vec4 vec4_P_M_Result = vec4_P_M + vec4_P_M2;
+    vec4_P_M_Result.printvec4();
+    vec3_P_M_Result = vec3_P_M - vec3_P_M2;
+    vec3_P_M_Result.printvec3();
+    vec4_P_M_Result = vec4_P_M - vec4_P_M2;
+    vec4_P_M_Result.printvec4();
+    vec1x3 vec1x3_P_M(4, 4, 4);
+    vec1x3 vec1x3_P_M2(3, 3, 3);
+    vec1x4 vec1x4_P_M(5, 5, 5, 5);
+    vec1x4 vec1x4_P_M2(4, 4, 4, 4);
+    vec1x3 vec1x3_P_M_Result = vec1x3_P_M + vec1x3_P_M2;
+    vec1x3_P_M_Result.printvec1x3();
+    vec1x4 vec1x4_P_M_Result = vec1x4_P_M + vec1x4_P_M2;
+    vec1x4_P_M_Result.printvec1x4();
+    vec1x3_P_M_Result = vec1x3_P_M - vec1x3_P_M2;
+    vec1x3_P_M_Result.printvec1x3();
+    vec1x4_P_M_Result = vec1x4_P_M - vec1x4_P_M2;
+    vec1x4_P_M_Result.printvec1x4();
+
+    //3. f-1) 행렬의 상수 곱셈, 나눗셈
     mat3_P_M_Result = mat3_P_M * 3;
     mat3_P_M_Result.printmat3();
     mat3_P_M_Result = 4*mat3_P_M;
@@ -309,4 +355,33 @@ int main()
     mat4_P_M_Result = mat4_P_M / 2;
     mat4_P_M_Result.printmat4();
     
+    //3. f-2) 벡터의 상수 곱셈, 나눗셈
+    vec3_P_M_Result = vec3_P_M * 3;
+    vec3_P_M_Result.printvec3();
+    vec3_P_M_Result = 4 * vec3_P_M;
+    vec3_P_M_Result.printvec3();
+    vec3_P_M_Result = vec3_P_M / 2;
+    vec3_P_M_Result.printvec3();
+
+    vec1x3_P_M_Result = vec1x3_P_M * 3;
+    vec1x3_P_M_Result.printvec1x3();
+    vec1x3_P_M_Result = 4 * vec1x3_P_M;
+    vec1x3_P_M_Result.printvec1x3();
+    vec1x3_P_M_Result = vec1x3_P_M / 2;
+    vec1x3_P_M_Result.printvec1x3();
+
+    vec4_P_M_Result = vec4_P_M * 3;
+    vec4_P_M_Result.printvec4();
+    vec4_P_M_Result = 4 * vec4_P_M;
+    vec4_P_M_Result.printvec4();
+    vec4_P_M_Result = vec4_P_M / 2;
+    vec4_P_M_Result.printvec4();
+
+    vec1x4_P_M_Result = vec1x4_P_M * 3;
+    vec1x4_P_M_Result.printvec1x4();
+    vec1x4_P_M_Result = 4 * vec1x4_P_M;
+    vec1x4_P_M_Result.printvec1x4();
+    vec1x4_P_M_Result = vec1x4_P_M / 2;
+    vec1x4_P_M_Result.printvec1x4();
+
 }
--- 11w/MusoenMath.h
+++ 11w/MusoenMath.h
@@ -1,12 +1,11 @@
 #pragma once
 
-
+//vec3 구조체
 struct vec3
 {
 	float x[3][1];
 	
-
-
+	//초기값
 	vec3()
 	{
 		x[0][0] = 0;
@@ -14,97 +13,106 @@ struct vec3
 		x[2][0] = 0;
 	}
 
-
+	//벡터값 직접 초기화
 	vec3(float x_, float y_, float z_)
 	{
 		x[0][0] = x_;
 		x[1][0] = y_;
 		x[2][0] = z_;
 	}
-
+	//프린트 함수 선언
 	void printvec3();
 };
 
-
-
+//vec1x3 구조체
 struct vec1x3
 {
 	float x[1][3];
 
+	//초기값
 	vec1x3()
 	{
 		x[0][0] = 0;
 		x[0][1] = 0;
 		x[0][2] = 0;
 	}
-
-
+	//직접 초기화
 	vec1x3(float x_, float y_, float z_)
 	{
 		x[0][0] = x_;
 		x[0][1] = y_;
 		x[0][2] = z_;
 	}
-
+	//프린트 함수
 	void printvec1x3();
 };
 
-
-
+// 두 벡터간 뺄셈 연산자 오버로딩
 vec3 operator-(vec3 a, vec3 b)
 {
 	vec3 result(a.x[0][0] - b.x[0][0], a.x[1][0] - b.x[1][0], a.x[2][0] - b.x[2][0]);
 	return result;
 }
-
+// 두 벡터간 덧셈 연산자 오버로딩
 vec3 operator+(vec3 a, vec3 b)
 {
 	vec3 result(a.x[0][0] + b.x[0][0], a.x[1][0] + b.x[1][0], a.x[2][0] + b.x[2][0]);
 	return result;
 }
 
-
+// 벡터*상수 연산자 오버로딩
+vec3 operator*(vec3 a, float b) {
+	vec3 result(a.x[0][0] * b, a.x[1][0] * b, a.x[2][0] * b);
+	return result;
+}
+// 상수*벡터 연산자 오버로딩
 vec3 operator*(float b, vec3 a ) {
 	vec3 result(a.x[0][0]*b, a.x[1][0] * b,a.x[2][0] * b);
 	return result;
 }
+
+// 벡터/상수 연산자 오버로딩
 vec3 operator/(vec3 a, float b) {
 	vec3 result(a.x[0][0] / b, a.x[1][0] / b, a.x[2][0] / b);
 	return result;
 }
 
-
+// 두 1x3벡터간 덧셈 연산자 오버로딩
 vec1x3 operator+(vec1x3 a, vec1x3 b)
 {
 	vec1x3 result(a.x[0][0] + b.x[0][0], a.x[0][1] + b.x[0][1], a.x[0][2] + b.x[0][2]);
 	return result;
 }
 
+// 두 1x3벡터간 뺄셈 연산자 오버로딩
 vec1x3 operator-(vec1x3 a, vec1x3 b)
 {
 	vec1x3 result(a.x[0][0] - b.x[0][0], a.x[0][1] - b.x[0][1], a.x[0][2] - b.x[0][2]);
 	return result;
 }
-
+// 상수*1x3벡터 연산자 오버로딩
 vec1x3 operator*(float a, vec1x3 b) {
 	vec1x3 result(b.x[0][0] * a, b.x[0][1] * a, b.x[0][2] * a);
 	return result;
 }
+// 1x3벡터*상수 연산자 오버로딩
 vec1x3 operator*(vec1x3 b,float a) {
 	vec1x3 result(b.x[0][0] * a, b.x[0][1] * a, b.x[0][2] * a);
 	return result;
 }
+
+// 1x3벡터/상수 연산자 오버로딩
 vec1x3 operator/(vec1x3 b, float a) {
 	vec1x3 result(b.x[0][0] / a, b.x[0][1] / a, b.x[0][2] / a);
 	return result;
 }
 
 
-
+// vec4 구조체
 struct vec4
 {
 	float x[4][1];
-
+	//초기값
 	vec4()
 	{
 		x[0][0] = 0;
@@ -113,7 +121,7 @@ struct vec4
 		x[3][0] = 0;
 	}
 
-
+	//직접 초기화
 	vec4(float x_, float y_, float z_, float w_)
 	{
 		x[0][0] = x_;
@@ -121,15 +129,45 @@ struct vec4
 		x[2][0] = z_;
 		x[3][0] = w_;
 	}
-
+	//프린트 함수
 	void printvec4();
 	
 };
 
+// 두 벡터간 뺄셈 연산자 오버로딩
+vec4 operator-(vec4 a, vec4 b)
+{
+	vec4 result(a.x[0][0] - b.x[0][0], a.x[1][0] - b.x[1][0], a.x[2][0] - b.x[2][0], a.x[3][0] - b.x[3][0]);
+	return result;
+}
+// 두 벡터간 덧셈 연산자 오버로딩
+vec4 operator+(vec4 a, vec4 b)
+{
+	vec4 result(a.x[0][0] + b.x[0][0], a.x[1][0] + b.x[1][0], a.x[2][0] + b.x[2][0], a.x[3][0] + b.x[3][0]);
+	return result;
+}
+// 상수*벡터 연산자 오버로딩
+vec4 operator*(float b, vec4 a) {
+	vec4 result(a.x[0][0] * b, a.x[1][0] * b, a.x[2][0] * b, a.x[3][0] * b);
+	return result;
+}
+// 벡터*상수 연산자 오버로딩
+vec4 operator*(vec4 a, float b) {
+	vec4 result(a.x[0][0] * b, a.x[1][0] * b, a.x[2][0] * b, a.x[3][0] * b);
+	return result;
+}// 벡터/상수 연산자 오버로딩
+
+vec4 operator/(vec4 a, float b) {
+	vec4 result(a.x[0][0] / b, a.x[1][0] / b, a.x[2][0] / b, a.x[3][0] / b);
+	return result;
+}
+
+//vec1x4 구조체
 struct vec1x4
 {
 	float x[1][4];
 
+	//초기값
 	vec1x4()
 	{
 		x[0][0] = 0;
@@ -138,7 +176,7 @@ struct vec1x4
 		x[0][3] = 0;
 	}
 
-
+	//직접 초기화
 	vec1x4(float x_, float y_, float z_ , float w_)
 	{
 		x[0][0] = x_;
@@ -146,78 +184,65 @@ struct vec1x4
 		x[0][2] = z_;
 		x[0][3] = w_;
 	}
-	
+	//프린트 함수
 	void printvec1x4();
 };
 
-vec4 operator-(vec4 a, vec4 b)
-{
-	vec4 result(a.x[0][0] - b.x[0][0], a.x[1][0] - b.x[1][0], a.x[2][0] - b.x[2][0], a.x[3][0] - b.x[3][0]);
-	return result;
-}
-
-vec4 operator+(vec4 a, vec4 b)
-{
-	vec4 result(a.x[0][0] + b.x[0][0], a.x[1][0] + b.x[1][0], a.x[2][0] + b.x[2][0], a.x[3][0] + b.x[3][0]);
-	return result;
-}
-
-
-vec4 operator*(float b, vec4 a) {
-	vec4 result(a.x[0][0] * b, a.x[1][0] * b, a.x[2][0] * b , a.x[3][0] *b);
-	return result;
-}
-vec4 operator*(vec4 a,float b) {
-	vec4 result(a.x[0][0] * b, a.x[1][0] * b, a.x[2][0] * b, a.x[3][0] * b);
-	return result;
-}
-vec4 operator/(vec4 a, float b) {
-	vec4 result(a.x[0][0] / b, a.x[1][0] / b, a.x[2][0] / b ,a.x[3][0]/b);
-	return result;
-}
-
-
-
-
-
+// 두 벡터간 덧셈 연산자 오버로딩
 vec1x4 operator+(vec1x4 a, vec1x4 b)
 {
 	vec1x4 result(a.x[0][0] + b.x[0][0], a.x[0][1] + b.x[0][1], a.x[0][2] + b.x[0][2] , a.x[0][3] + b.x[0][3]);
 	return result;
 }
 
+// 두 벡터간 뺄셈 연산자 오버로딩
 vec1x4 operator-(vec1x4 a, vec1x4 b)
 {
 	vec1x4 result(a.x[0][0] - b.x[0][0], a.x[0][1] - b.x[0][1], a.x[0][2] - b.x[0][2], a.x[0][3] - b.x[0][3]);
 	return result;
 }
 
+// 벡터*상수 연산자 오버로딩
+vec1x4 operator*(vec1x4 b, float a) {
+	vec1x4 result(b.x[0][0] * a, b.x[0][1] * a, b.x[0][2] * a, b.x[0][3] * a);
+	return result;
+}
+
+// 상수*벡터 연산자 오버로딩
 vec1x4 operator*(float a, vec1x4 b) {
 	vec1x4 result(b.x[0][0] * a, b.x[0][1] * a, b.x[0][2] * a , b.x[0][3] * a);
 	return result;
 }
+
+// 벡터/상수 연산자 오버로딩
 vec1x4 operator/(vec1x4 b, float a) {
 	vec1x4 result(b.x[0][0] / a, b.x[0][1] / a, b.x[0][2] / a , b.x[0][3]/a);
 	return result;
 }
 
+//mat3 구조체
 struct mat3
 {
 	float a[3][3];
 
+	
+	//초기값
 	mat3()
 	{
+		//1행
 		a[0][0] = 0;
 		a[0][1] = 0;
 		a[0][2] = 0;
+		//2행
 		a[1][0] = 0;
 		a[1][1] = 0;
 		a[1][2] = 0;
+		//3행
 		a[2][0] = 0;
 		a[2][1] = 0;
 		a[2][2] = 0;
 	}
-
+	//초기화
 	mat3(float a_, float b_, float c_, float d_, float e_, float f_, float g_, float h_, float i_)
 	{
 		a[0][0] = a_;
@@ -232,34 +257,42 @@ struct mat3
 		a[2][1] = h_;
 		a[2][2] = i_;
 	}
+	//대입 연산자 오버로딩
 	void operator=(const mat3& a);
+
+	//프린트 함수
 	void printmat3();
+
+	//단위행렬 초기화 함수 선언
 	void Mat3identity();
+
+	//전치함수 선언
 	void Mat3Transpose();
+
+	// 전치 후 곱셈 함수 선언
 	void multiplytransposematrix(mat3 x , mat3 y);
 };
 
-
-
-
+//행렬간 곱셈 연산 연산자 오버로딩
 mat3 operator*(mat3 a, mat3 b)
 {
 	mat3 r;
-
+	//1행
 	r.a[0][0] = (a.a[0][0] * b.a[0][0]) + (a.a[0][1] * b.a[1][0]) + (a.a[0][2] * b.a[2][0]);
 	r.a[0][1] = (a.a[0][0] * b.a[0][1]) + (a.a[0][1] * b.a[1][1]) + (a.a[0][2] * b.a[2][1]);
 	r.a[0][2] = (a.a[0][0] * b.a[0][2]) + (a.a[0][1] * b.a[1][2]) + (a.a[0][2] * b.a[2][2]);
-
+	//2행
 	r.a[1][0] = (a.a[1][0] * b.a[0][0]) + (a.a[1][1] * b.a[1][0]) + (a.a[1][2] * b.a[2][0]);
 	r.a[1][1] = (a.a[1][0] * b.a[0][1]) + (a.a[1][1] * b.a[1][1]) + (a.a[1][2] * b.a[2][1]);
 	r.a[1][2] = (a.a[1][0] * b.a[0][2]) + (a.a[1][1] * b.a[1][2]) + (a.a[1][2] * b.a[2][2]);
-
+	//3행
 	r.a[2][0] = (a.a[2][0] * b.a[0][0]) + (a.a[2][1] * b.a[1][0]) + (a.a[2][2] * b.a[2][0]);
 	r.a[2][1] = (a.a[2][0] * b.a[0][1]) + (a.a[2][1] * b.a[1][1]) + (a.a[2][2] * b.a[2][1]);
 	r.a[2][2] = (a.a[2][0] * b.a[0][2]) + (a.a[2][1] * b.a[1][2]) + (a.a[2][2] * b.a[2][2]);
 	return r;
 }
 
+// 상수와 3x3행렬 곱셈 연산자 오버로딩
 mat3 operator*(float a, mat3 b) {
 	
 	mat3 r;
@@ -268,6 +301,8 @@ mat3 operator*(float a, mat3 b) {
 			r.a[i][j] =a* b.a[i][j];
 	return r;
 }
+
+// 행렬과 상수 곱셈 연산자 오버로딩
 mat3 operator*( mat3 b, float a) {
 
 	mat3 r;
@@ -276,6 +311,8 @@ mat3 operator*( mat3 b, float a) {
 			r.a[i][j] = b.a[i][j] * a;
 	return r;
 }
+
+// 행렬 / 상수 연산자 오버로딩
 mat3 operator/(mat3 b,float a) {
 	
 	mat3 r;
@@ -284,6 +321,8 @@ mat3 operator/(mat3 b,float a) {
 			r.a[i][j] = b.a[i][j]/a;
 	return r;
 }
+
+// 두 행렬간 덧셈 연산자 오버로딩
 mat3 operator+(mat3 a, mat3 b) {
 	mat3 r;
 
@@ -293,6 +332,8 @@ mat3 operator+(mat3 a, mat3 b) {
 
 	return r;
 }
+
+// 두 행렬간 뺄셈 연산자 오버로딩
 mat3 operator-(mat3 a, mat3 b) {
 	mat3 r;
 
@@ -302,77 +343,118 @@ mat3 operator-(mat3 a, mat3 b) {
 	return r;
 }
 
+//행렬과 3x1벡터의 곱셈 연산자 오버로딩
 vec3 operator*(mat3 b, vec3 a)
 {
-	
+	//결과값
 	vec3 r;
+
+	//1행
 	r.x[0][0] = (a.x[0][0] * b.a[0][0]) + (a.x[1][0] * b.a[0][1]) + (a.x[2][0] * b.a[0][2]);
+	//2행
 	r.x[1][0] = (a.x[0][0] * b.a[1][0]) + (a.x[1][0] * b.a[1][1]) + (a.x[2][0] * b.a[1][2]);
+	//3행
 	r.x[2][0] = (a.x[0][0] * b.a[2][0]) + (a.x[1][0] * b.a[2][1]) + (a.x[2][0] * b.a[2][2]);
 	return r;
 }
 
+// 행렬과 3x1벡터의 곱셈 순서가 바뀌었을 때 vec3x1과 행렬을 각각 전치하여 계산하는 연산자 오버로딩
 vec3 operator*(vec3 a, mat3 b)
 {
+	//행렬 전치
 	b.Mat3Transpose();
+
+	//결과값
 	vec3 r;
+
+	//전치 대상
 	vec1x3 a1;
+	
+	//벡터 전치 진행
 	for (int i = 0; i < 3; i++)
 			a1.x[0][i] = a.x[i][0];
+
+	//1행
 	r.x[0][0] = (a1.x[0][0] * b.a[0][0]) + (a1.x[0][1] * b.a[1][0]) + (a1.x[0][2] * b.a[2][0]);
+	//2행
 	r.x[0][1] = (a1.x[0][0] * b.a[0][1]) + (a1.x[0][1] * b.a[1][1]) + (a1.x[0][2] * b.a[2][1]);
+	//3행
 	r.x[0][2] = (a1.x[0][0] * b.a[0][2]) + (a1.x[0][1] * b.a[1][2]) + (a1.x[0][2] * b.a[2][2]);
 	return r;
 }
 
+//1x3벡터와 행렬간 곱셈 연산자 오버로딩. 연산이 전치 후 진행된다.
 vec1x3 operator*(vec1x3 b, mat3 a)
 {
+	//행렬 전치
 	a.Mat3Transpose();
+
+	//결과값
 	vec1x3 r;
+
+	//1열
 	r.x[0][0] = (b.x[0][0] * a.a[0][0]) + (b.x[0][1] * a.a[1][0]) + (b.x[0][2] * a.a[2][0]);
+	//2열
 	r.x[0][1] = (b.x[0][0] * a.a[0][1]) + (b.x[0][1] * a.a[1][1]) + (b.x[0][2] * a.a[2][1]);
+	//3열
 	r.x[0][2] = (b.x[0][0] * a.a[0][2]) + (b.x[0][1] * a.a[1][2]) + (b.x[0][2] * a.a[2][2]);
 	return r;
 }
+// 행렬과 1x3벡터의 곱셈 순서가 바뀌었을 때 vec1x3을 vec3로 전치 후 연산을 진행하는 연산자 오버로딩
 vec1x3 operator*(mat3 b,vec1x3 a)
 {
+	//결과값
 	vec1x3 r;
+
+	//전치 대상
 	vec3 a1;
+
+	//벡터 전치 진행
 	for (int i = 0; i < 3; i++)
 		a1.x[i][0] = a.x[0][i];
 
+	//1열
 	r.x[0][0] = (a1.x[0][0] * b.a[0][0]) + (a1.x[1][0] * b.a[0][1]) + (a1.x[2][0] * b.a[0][2]);
+	//2열
 	r.x[0][1] = (a1.x[0][0] * b.a[1][0]) + (a1.x[1][0] * b.a[1][1]) + (a1.x[2][0] * b.a[1][2]);
+	//3열
 	r.x[0][2] = (a1.x[0][0] * b.a[2][0]) + (a1.x[1][0] * b.a[2][1]) + (a1.x[2][0] * b.a[2][2]);
 	return r;
 }
 
+//mat4 구조체
 struct mat4
 {
 	float b[4][4];
 
+	//초기값 0
 	mat4() {
+		//1행
 		b[0][0] = 0;
 		b[0][1] = 0;
 		b[0][2] = 0;
 		b[0][3] = 0;
 
+		//2행
 		b[1][0] = 0;
 		b[1][1] = 0;
 		b[1][2] = 0;
 		b[1][3] = 0;
 
+		//3행
 		b[2][0] = 0;
 		b[2][1] = 0;
 		b[2][2] = 0;
 		b[2][3] = 0;
 
+		//4헹
 		b[3][0] = 0;
 		b[3][1] = 0;
 		b[3][2] = 0;
 		b[3][3] = 0;
 	}
 
+	//값 초기화
 	mat4(float m4_1, float m4_2, float m4_3, float m4_4, float m4_5, float m4_6, float m4_7, float m4_8, float m4_9, float m4_10, float m4_11, float m4_12, float m4_13, float m4_14, float m4_15, float m4_16) {
 		b[0][0] = m4_1;
 		b[0][1] = m4_2;
@@ -394,34 +476,43 @@ struct mat4
 		b[3][2] = m4_15;
 		b[3][3] = m4_16;
 	}
+	//전치 함수
 	void Mat4Transpose();
+
+	// 프린트 함수
 	void printmat4();
+
+	// 단위행렬 초기화 함수
 	void Mat4Identity();
+
+	//대입 연산자 오버로딩
 	void operator=(const mat4& a);
+
+	//전치 후 곱셈을 진행하는 함수 선언
 	void multiplytransposematrix(mat4 a, mat4 b);
 };
 
 
-
+//두 행렬간 곱셈 연산자 오버로딩
 mat4 operator*(mat4 a, mat4 b)
 {
 	mat4 r;
-
+	//1행
 	r.b[0][0] = (a.b[0][0] * b.b[0][0]) + (a.b[0][1] * b.b[1][0]) + (a.b[0][2] * b.b[2][0]) + (a.b[0][3] * b.b[3][0]);
 	r.b[0][1] = (a.b[0][0] * b.b[0][1]) + (a.b[0][1] * b.b[1][1]) + (a.b[0][2] * b.b[2][1]) + (a.b[0][3] * b.b[3][1]);
 	r.b[0][2] = (a.b[0][0] * b.b[0][2]) + (a.b[0][1] * b.b[1][2]) + (a.b[0][2] * b.b[2][2]) + (a.b[0][3] * b.b[3][2]);
 	r.b[0][3] = (a.b[0][0] * b.b[0][3]) + (a.b[0][1] * b.b[1][3]) + (a.b[0][2] * b.b[2][3]) + (a.b[0][3] * b.b[3][3]);
-
+	//2행
 	r.b[1][0] = (a.b[1][0] * b.b[0][0]) + (a.b[1][1] * b.b[1][0]) + (a.b[1][2] * b.b[2][0]) + (a.b[1][3] * b.b[3][0]);
 	r.b[1][1] = (a.b[1][0] * b.b[0][1]) + (a.b[1][1] * b.b[1][1]) + (a.b[1][2] * b.b[2][1]) + (a.b[1][3] * b.b[3][1]);
 	r.b[1][2] = (a.b[1][0] * b.b[0][2]) + (a.b[1][1] * b.b[1][2]) + (a.b[1][2] * b.b[2][2]) + (a.b[1][3] * b.b[3][2]);
 	r.b[1][3] = (a.b[1][0] * b.b[0][3]) + (a.b[1][1] * b.b[1][3]) + (a.b[1][2] * b.b[2][3]) + (a.b[1][3] * b.b[3][3]);
-
+	//3행
 	r.b[2][0] = (a.b[2][0] * b.b[0][0]) + (a.b[2][1] * b.b[1][0]) + (a.b[2][2] * b.b[2][0]) + (a.b[2][3] * b.b[3][0]);
 	r.b[2][1] = (a.b[2][0] * b.b[0][1]) + (a.b[2][1] * b.b[1][1]) + (a.b[2][2] * b.b[2][1]) + (a.b[2][3] * b.b[3][1]);
 	r.b[2][2] = (a.b[2][0] * b.b[0][2]) + (a.b[2][1] * b.b[1][2]) + (a.b[2][2] * b.b[2][2]) + (a.b[2][3] * b.b[3][2]);
 	r.b[2][3] = (a.b[2][0] * b.b[0][3]) + (a.b[2][1] * b.b[1][3]) + (a.b[2][2] * b.b[2][3]) + (a.b[2][3] * b.b[3][3]);
-
+	//4행
 	r.b[3][0] = (a.b[3][0] * b.b[0][0]) + (a.b[3][1] * b.b[1][0]) + (a.b[3][2] * b.b[2][0]) + (a.b[3][3] * b.b[3][0]);
 	r.b[3][1] = (a.b[3][0] * b.b[0][1]) + (a.b[3][1] * b.b[1][1]) + (a.b[3][2] * b.b[2][1]) + (a.b[3][3] * b.b[3][1]);
 	r.b[3][2] = (a.b[3][0] * b.b[0][2]) + (a.b[3][1] * b.b[1][2]) + (a.b[3][2] * b.b[2][2]) + (a.b[3][3] * b.b[3][2]);
@@ -430,61 +521,94 @@ mat4 operator*(mat4 a, mat4 b)
 	return r;
 }
 
-
+// 행렬과 벡터간 곱셈 연산자 오버로딩
 vec4 operator*(mat4 b, vec4 a)
 {
 	
 	vec4 r;
+	//1행
 	r.x[0][0] = (a.x[0][0] * b.b[0][0]) + (a.x[1][0] * b.b[0][1]) + (a.x[2][0] * b.b[0][2]) + (a.x[3][0] * b.b[0][3]);
+	//2행
 	r.x[1][0] = (a.x[0][0] * b.b[1][0]) + (a.x[1][0] * b.b[1][1]) + (a.x[2][0] * b.b[1][2]) + (a.x[3][0] * b.b[1][3]);
+	//3행
 	r.x[2][0] = (a.x[0][0] * b.b[2][0]) + (a.x[1][0] * b.b[2][1]) + (a.x[2][0] * b.b[2][2]) + (a.x[3][0] * b.b[2][3]);
+	//4행
 	r.x[3][0] = (a.x[0][0] * b.b[3][0]) + (a.x[1][0] * b.b[3][1]) + (a.x[2][0] * b.b[3][2]) + (a.x[3][0] * b.b[3][3]);
 	return r;
 }
+
+//  행렬과 벡터의 곱셈의 순서가 바뀌었을 때 연산자 오버로딩 각각 행렬과 벡터를 전치 후 연산을 진행하는 연산자 오버로딩
 vec4 operator*(vec4 b, mat4 a)
 {
+	//행렬 전치
 	a.Mat4Transpose();
+
+	//전치 대상
 	vec1x4 b1;
+
+	//연산 결과 값 
 	vec4 r;
+
+	//벡터 전치 진행
 	for (int i = 0; i < 4; i++)
 		b1.x[0][i] = b.x[i][0];
 
+	//1행
 	r.x[0][0] = (b1.x[0][0] * a.b[0][0]) + (b1.x[0][1] * a.b[1][0]) + (b1.x[0][2] * a.b[2][0]) + (b1.x[0][3] * a.b[3][0]);
+	//2행
 	r.x[1][0] = (b1.x[0][0] * a.b[0][1]) + (b1.x[0][1] * a.b[1][1]) + (b1.x[0][2] * a.b[2][1]) + (b1.x[0][3] * a.b[3][1]);
+	//3행
 	r.x[2][0] = (b1.x[0][0] * a.b[0][2]) + (b1.x[0][1] * a.b[1][2]) + (b1.x[0][2] * a.b[2][2]) + (b1.x[0][3] * a.b[3][2]);
+	//4행
 	r.x[3][0] = (b1.x[0][0] * a.b[0][3]) + (b1.x[0][1] * a.b[1][3]) + (b1.x[0][2] * a.b[2][3]) + (b1.x[0][3] * a.b[3][3]);
 	return r;
 }
 
-
+// 1x4벡터와 행렬간 곱셈 연산자 오버로딩. 행렬을 전치 후 연산을 진행한다.
 vec1x4 operator*(vec1x4 b, mat4 a)
 {
+	//행렬 전치
 	a.Mat4Transpose();
+
+	//결과 값
 	vec1x4 r;
+
+	//1열
 	r.x[0][0] = (b.x[0][0] * a.b[0][0]) + (b.x[0][1] * a.b[1][0]) + (b.x[0][2] * a.b[2][0]) + (b.x[0][3] * a.b[3][0]);
+	//2열
 	r.x[0][1] = (b.x[0][0] * a.b[0][1]) + (b.x[0][1] * a.b[1][1]) + (b.x[0][2] * a.b[2][1]) + (b.x[0][3] * a.b[3][1]);
+	//열
 	r.x[0][2] = (b.x[0][0] * a.b[0][2]) + (b.x[0][1] * a.b[1][2]) + (b.x[0][2] * a.b[2][2]) + (b.x[0][3] * a.b[3][2]);
+	//4열
 	r.x[0][3] = (b.x[0][0] * a.b[0][3]) + (b.x[0][1] * a.b[1][3]) + (b.x[0][2] * a.b[2][3]) + (b.x[0][3] * a.b[3][3]);
 	return r;
 }
+
+// 행렬과 1x4벡터간 곰셈이 순서가 바뀌었을 경우 연산자 오버로딩. 벡터1x4을 vec4로 전치 후 연산을 진행한다.
 vec1x4 operator*(mat4 b, vec1x4 a)
 {
-	
+	//전치 대상
 	vec4 a1;
+
+	//결과값
 	vec1x4 r;
+
+	//벡터 전치 진행
 	for (int i = 0; i < 4; i++)
 		a1.x[i][0] = a.x[0][i];
 
+	//1열
 	r.x[0][0] = (a1.x[0][0] * b.b[0][0]) + (a1.x[1][0] * b.b[0][1]) + (a1.x[2][0] * b.b[0][2]) + (a1.x[3][0] * b.b[0][3]);
+	//2열
 	r.x[0][1] = (a1.x[0][0] * b.b[1][0]) + (a1.x[1][0] * b.b[1][1]) + (a1.x[2][0] * b.b[1][2]) + (a1.x[3][0] * b.b[1][3]);
+	//3열
 	r.x[0][2] = (a1.x[0][0] * b.b[2][0]) + (a1.x[1][0] * b.b[2][1]) + (a1.x[2][0] * b.b[2][2]) + (a1.x[3][0] * b.b[2][3]);
+	//4열
 	r.x[0][3] = (a1.x[0][0] * b.b[3][0]) + (a1.x[1][0] * b.b[3][1]) + (a1.x[2][0] * b.b[3][2]) + (a1.x[3][0] * b.b[3][3]);
 	return r;
 }
 
-
-
-
+//상수와 행렬 곱셈 연산자 오버로딩
 mat4 operator*(float a, mat4 b) {
 	
 	mat4 r;
@@ -495,6 +619,7 @@ mat4 operator*(float a, mat4 b) {
 
 	return r;
 }
+// 행렬과 상수 곱셈 연산자 오버로딩
 mat4 operator*(mat4 b, float a) {
 
 	mat4 r;
@@ -505,6 +630,7 @@ mat4 operator*(mat4 b, float a) {
 
 	return r;
 }
+// 행렬과 상수 나눗셈 연산자 오버로딩
 mat4 operator/(mat4 b ,float a ) {
 	
 	mat4 r;
@@ -514,6 +640,7 @@ mat4 operator/(mat4 b ,float a ) {
 
 	return r;
 }
+// 두 행렬간 덧셈 연산자 오버로딩
 mat4 operator+(mat4 a, mat4 b) {
 	mat4 r;
 
@@ -522,6 +649,8 @@ mat4 operator+(mat4 a, mat4 b) {
 			r.b[i][j] = a.b[i][j] + b.b[i][j];
 	return r;
 }
+
+// 두 행렬간 뺄셈 연산자 오버로딩
 mat4 operator-(mat4 a, mat4 b) {
 	mat4 r;
 
